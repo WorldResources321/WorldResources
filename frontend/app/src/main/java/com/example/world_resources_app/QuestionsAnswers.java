293a7dce7733254b9a025e76075df6d2be1f4d00
Michael14265 <michael.alexander.perkins@gmail.com>
2022-07-12 17:29:10 -0700

    Integrated Project with All Current Features

---
+++ frontend/app/src/main/java/com/example/world_resources_app/QuestionsAnswers.java
@@ -0,0 +1,44 @@
+package com.example.world_resources_app;
+
+public class QuestionsAnswers {
+
+    public static String question[] ={
+            "Which country produces the most fossil fuels?",
+            "Which US state is the biggest contributor of fossil fuels?",
+            "What is the most widely used fossil fuel currently?",
+            "Which country is the biggest supplier of fossil fuels for Europe?",
+            "Where does Canada get most of its energy from?",
+            "Who is the largest producer of Natural Gas?",
+            "Where does China get most of its energy from?",
+            "Which of the listed countries has the highest GDP per capita in the world?",
+            "Which country was the first to attempt an industrial scale carbon capture and storage project?",
+            "Where does Australia get most of its energy from?"
+    };
+
+    public static String choices[][] = {
+            {"Canada","USA","Russia","Iran"},
+            {"California","Wyoming","Mississippi","Texas"},
+            {"Oil","Coal","Natural Gas","Petrol"},
+            {"Russia","Norway","China","Algeria"},
+            {"Nuclear","Oil","Hydro","Coal"},
+            {"Qatar","Iran","USA","Russia"},
+            {"Nuclear","Oil","Hydro","Coal"},
+            {"Qatar","Saudi Arabia","USA","Norway"},
+            {"Canada","USA","Norway","Qatar"},
+            {"Nuclear","Oil","Hydro","Coal"}
+    };
+
+    public static String correctAnswers[] = {
+            "USA",
+            "Texas",
+            "Oil",
+            "Russia",
+            "Hydro",
+            "Iran",
+            "Coal",
+            "Qatar",
+            "Norway",
+            "Coal"
+    };
+
+}
