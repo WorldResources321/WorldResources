3f89b1b476295cc77cd99c71e31f56b7dbfe0b0b
karatwan <71236899+karatwan@users.noreply.github.com>
2022-07-15 02:47:57 -0700

    Create getMockReported.js

---
+++ backend/mock/getMockReported.js
@@ -0,0 +1,14 @@
+//mocked module (stores reported users)
+
+function getMockReported() {
+    var reportedUsers = [];
+    reportedUsers.push("aaa@gmail.com");
+    reportedUsers.push("bbb@gmail.com");
+    reportedUsers.push("ccc@gmail.com");
+    reportedUsers.push("ddd@gmail.com");
+    reportedUsers.push("eee@gmail.com");
+    reportedUsers.push("fff@gmail.com");
+    reportedUsers.push("ggg@gmail.com");
+    return reportedUsers;
+}
+module.exports = getMockReported;
