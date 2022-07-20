b4a22a8f73ff59465cbde82c17547800ade26779
karatwan <71236899+karatwan@users.noreply.github.com>
2022-07-19 18:05:30 -0700

    Update ButtonTest.java

--- frontend/app/src/androidTest/java/com/example/world_resources_app/ButtonTest.java
+++ frontend/app/src/androidTest/java/com/example/world_resources_app/ButtonTest.java
@@ -3,7 +3,7 @@ package com.example.world_resources_app;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+//import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -11,7 +11,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import android.content.Context;
+//import android.content.Context;
 
 import androidx.test.filters.LargeTest;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
