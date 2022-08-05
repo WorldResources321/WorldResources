ff59e7f5e86e7776c9501289a17cc96becea0d37
karatwan <karat.wannissorn@gmail.com>
2022-08-05 09:38:20 -0700

    mock test block user

---
+++ backend/blockUserMock.js
@@ -0,0 +1,24 @@
+const getAllUsers = require('./getAllUsers')
+const getBlockedUsers = require('./getBlockedUsers')
+
+//save user to database if user is "valid" and return 0
+function blockUser (user) {
+
+    var mockAllUsers = getAllUsers();
+    var mockBlockedUsers = getBlockedUsers();
+
+    if (user === "") { //user given is empty
+        return {status: 400, message: "user unspecified"};
+    }
+    else if (mockAllUsers.indexOf(user) == -1) { //invalid user
+        return {status: 400, message: "user does not exist"};
+    }
+    else if (mockBlockedUsers.indexOf(user) > -1) { //user is already blocked
+        return {status: 400, message: "user is already blocked"};
+    }
+    else { //valid user
+        return {status: 200, message: "user blocked"};
+    }
+
+}
+module.exports = blockUser;
--- backend/mock_tests/blockUser.test.js
+++ backend/mock_tests/blockUser.test.js
@@ -1,4 +1,4 @@
-const blockUser = require('../blockUser')
+const blockUser = require('../blockUserMock')
 
 jest.mock("../getAllUsers.js")
 jest.mock("../getBlockedUsers.js")
@@ -7,22 +7,22 @@ describe('mocking user database', () => {
 
     var resultInt1 = blockUser("")
     test('correct output when user given is empty', () => {
-        expect (resultInt1).toBe(-1);
+        expect (resultInt1).toMatchObject({status: 400, message: "user unspecified"});
     });
 
     var resultInt2 = blockUser("dne@gmail.com")
     test('correct output when user given does not exist', () => {
-        expect (resultInt2).toBe(-1);
+        expect (resultInt2).toMatchObject({status: 400, message: "user does not exist"});
     });
 
     var resultInt3 = blockUser("blocked111@gmail.com")
     test('correct output when user given is already blocked', () => {
-        expect (resultInt3).toBe(-1);
+        expect (resultInt3).toMatchObject({status: 400, message: "user is already blocked"});
     });
 
     var resultInt4 = blockUser("aaa@gmail.com")
     test('correct output when user given is valid', () => {
-        expect (resultInt4).toBe(0);
+        expect (resultInt4).toMatchObject({status: 200, message: "user blocked"});
     });
 
 });
