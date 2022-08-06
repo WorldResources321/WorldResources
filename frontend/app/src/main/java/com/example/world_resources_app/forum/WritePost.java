32219c96cf70e116a1f07cf0a914ca7b6d3492fc
karatwan <karat.wannissorn@gmail.com>
2022-08-06 08:43:45 -0700

    codacy

--- frontend/app/src/main/java/com/example/world_resources_app/forum/WritePost.java
+++ frontend/app/src/main/java/com/example/world_resources_app/forum/WritePost.java
@@ -9,16 +9,9 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.NetworkError;
-import com.android.volley.NoConnectionError;
-import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.ServerError;
-import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
