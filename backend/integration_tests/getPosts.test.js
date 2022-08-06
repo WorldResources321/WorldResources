d13affa84b681698045a9c054c55d0d58d4b0aac
karatwan <karat.wannissorn@gmail.com>
2022-08-06 07:30:52 -0700

    update getposts test

--- backend/integration_tests/getPosts.test.js
+++ backend/integration_tests/getPosts.test.js
@@ -22,45 +22,19 @@ describe('get posts', () => {
         await client.close()    
     })
 
-    it('empty forum (no post attempts)', async() => {
+    it('empty forum', async() => {
         await request(app)
             .get('/getPosts')
             .expect(404);
 
     });
 
-    it('empty forum (after posting with empty content)', async() => {
-        await request(app)
-            .post('/postToforum')
-            .send({content: '', author: 'smith@gmail.com'});
+    it('non-empty forum', async() => {
 
         await request(app)
-            .get('/getPosts')
-            .expect(404);
+            .post('/addUser')
+            .send({email: 'user1@gmail.com'});
 
-    });
-
-    it('empty forum (after posting with blocked user', async() => {
-        await request(app)
-            .post('/posttoforum')
-            .send({content: 'blahbalh', author: 'andrew@gmail.com'});
-        await request(app)
-            .get('/getPosts')
-            .expect(404);
-
-    });
-
-    it('empty forum (after posting with non-existing user', async() => {
-        await request(app)
-            .post('/posttoforum')
-            .send({content: 'blahbalh', author: 'doesnotexist@gmail.com'});
-        await request(app)
-            .get('/getPosts')
-            .expect(404);
-
-    });
-
-    it('non-empty forum (after posting with valid user)', async() => {
         await request(app)
             .post('/postToForum')
             .send({content: 'balahblah', author: 'user1@gmail.com'});
